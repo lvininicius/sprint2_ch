3d1eee036ff3e80e9ce14ad15c86eb70ee697563
Lucas Vinicius <lvininicius@gmail.com>
2023-05-14 11:53:19 -0300

    subindo versão programa python_2.0

---
+++ challenge.py
@@ -0,0 +1,80 @@
+
+def opcao1():
+    
+    global dados
+
+    print("Você selecionou a opção (Registrar um reciclador).")
+    # Obtém o número do container, a rua, cidade e estado através de input do usuário
+    a = int(input("Digite o número do Reciclador: "))
+    b = str(input("Digite a rua do Reciclador: "))
+    c = str(input("Digite a cidade: "))
+    print("## Reciclador registrado ##")
+    dados=[c,b]
+    
+
+def opcao2(dados):
+    
+    import requests
+    
+    b,c = dados
+    print("Você selecionou a opção 2.")
+
+    # URL da API do OpenStreetMap para obter as coordenadas geográficas a partir do endereço
+    url = 'https://nominatim.openstreetmap.org/search?q={b},{c}&format=json&limit=1'
+
+    # Faz uma requisição GET para a URL definida acima, com um timeout de 10 segundos para receber a resposta
+    response = requests.get(url, timeout=(None, 10))
+
+    # Biblioteca para geolocalização (conversão de endereços em coordenadas)
+    from geopy.geocoders import Nominatim
+
+    # Biblioteca para plotar os pontos no mapa
+    import matplotlib.pyplot as plt
+
+    # Cria um objeto geolocator com um user_agent personalizado para usar a API do OpenStreetMap
+    geolocator = Nominatim(user_agent="my_app")
+
+    # Cria uma lista com as localizações a serem buscadas
+    locations = [f"{b},{c},"]
+
+    # Cria uma lista vazia para guardar as coordenadas geográficas de cada localização
+    coordinates = []
+
+    # Loop para obter as coordenadas geográficas de cada localização usando o objeto geolocator e a API do OpenStreetMap
+    for location in locations:
+        loc = geolocator.geocode(location)
+        coordinates.append((loc.latitude, loc.longitude))
+
+    # Cria um gráfico de dispersão com as coordenadas geográficas obtidas acima
+    plt.scatter([coord[1] for coord in coordinates], [coord[0] for coord in coordinates]) 
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.title("Locais em São Paulo")
+    plt.show()
+
+def opcao3():
+    
+    print("Você selecionou a opção 3.")
+    # Coloque os comandos relacionados à opção 3 aqui
+
+while  True:
+    print("_____________________________")
+    print("Selecione uma opção:")
+    print("1. Registrar um reciclador")
+    print("2. Localizar um reciclador")
+    print("3. Opção 3")
+    print("4. Sair")
+    print("_____________________________")
+
+    opcao = input("Digite o número da opção desejada: ")
+    if opcao == "1":
+        opcao1()
+    elif opcao == "2":
+        opcao2(dados)
+    elif opcao == "3":
+        opcao3()
+    elif opcao == "4":
+        print("Tchauuuuuu!")
+        break
+    else:
+        print("Opção Invalida")
---
+++ readme.txt
@@ -0,0 +1,18 @@
+*Processo para Linux
+*Para voce iniciar o codigo, voce precisa executar os seguintes comandos
+
+1 - apt install pip
+2 - acesse o diretorio onde está o codigo e seu arquivo
+3 - execute o comando pip install -r config-start-linux.txt
+
+Cocluido, apenas execute o codigo de geolocalização
+
+*Processo para Windowns
+1 - acesse o site (https://www.python.org/) e realize o download da ultima versão do python.
+2 - abra o seu power shell executando como admistrador.
+3 - Acesse a pasta onde está o arquivo das bibliotecas "config-start-win.txt"
+4 - Execute o comando pip install -r config-start-win.txt
+
+Cocluido, apenas execute o codigo de geolocalização 
+
+O desenvolvimento dese codigo foi realizado para o projeto da challenge IBM em 2023 da faculdade FIAP,a intenção do mesmo é registrar e indentificar os containers  inteligentes (TYBA.I.O.T) e identificalos ultilizando bibliotecas de geolocalização.
